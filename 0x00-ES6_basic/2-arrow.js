de84c5751344822d7b566c26247edf4fe9f634ba
faith2221 <edithbanda014@gmail.com>
2024-04-22 11:38:21 +0000

    Corrections

--- 0x00-ES6_basic/2-arrow.js
+++ 0x00-ES6_basic/2-arrow.js
@@ -2,7 +2,7 @@ export default function getNeighborhoodsList() {
   this.sanFranciscoNeighborhoods = ['SOMA', 'Union Square'];
 
   const self = this;
-  this.addNeighborhood = function add(newNeighborhood) {
+  this.addNeighborhood = (newNeighborhood) => {
     this.sanFranciscoNeighborhoods.push(newNeighborhood);
     return this.sanFranciscoNeighborhoods;
   };
