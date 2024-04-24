79d1f4224068a63a62800ca64de3a4308586a836
faith2221 <edithbanda014@gmail.com>
2024-04-24 08:40:44 +0000

    Correction

--- 0x01-ES6_promise/1-promise.js
+++ 0x01-ES6_promise/1-promise.js
@@ -1,9 +1,13 @@
 export default function getFullResponseFromAPI(success) {
-  return new Promise((resolve, reject) => {
+  const thePromise = new Promise((resolve, reject) => {
     if (success) {
-      resolve({ status: 200, body: 'Success'});
-    } else {
-      reject(new Error('The fake API is not working currently'));
-    }
+      resolve({
+        status: 200,
+	body: 'Success',
+      });
+     } else {
+       reject(new Error('The fake API is not working currently'));
+     }
   });
+  return (thePromise);
 }
