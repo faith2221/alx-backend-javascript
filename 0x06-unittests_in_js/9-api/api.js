44933ab626302fab80608de1444a8fa16d88e4c2
faith2221 <edithbanda014@gmail.com>
2024-06-20 06:41:40 +0000

    Correction to Task 9  Regex integration testing

--- 0x06-unittests_in_js/9-api/api.js
+++ 0x06-unittests_in_js/9-api/api.js
@@ -8,9 +8,11 @@ app.get('/', (req, res) => {
 });
 
 app.get('/cart/:id([0-9]+)', (req, res) => {
-    res.end('Payment methods for cart ${req.params.id}');
+    res.end(`Payment methods for cart ${req.params.id}`);
 });
 
 app.listen(port, () => {
     console.log('API available on localhost port 7865');
 });
+
+module.exports = app;
